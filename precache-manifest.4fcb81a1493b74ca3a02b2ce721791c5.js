b416fa30fe00882544ff62a11d6abd33c7a99e3f
SaiAshish9 <saiashish3760@gmail.com>
2020-03-29 06:46:38 +0530

    Updates

---
+++ precache-manifest.4fcb81a1493b74ca3a02b2ce721791c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e56dbd81de882d438e6e40b9cc20fc",
+    "url": "/fitby/index.html"
+  },
+  {
+    "revision": "a40cc8b932a99befdc43",
+    "url": "/fitby/static/css/main.ef8506fe.chunk.css"
+  },
+  {
+    "revision": "50a2d65e962ca3795f39",
+    "url": "/fitby/static/js/2.0b0a8ee8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitby/static/js/2.0b0a8ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a40cc8b932a99befdc43",
+    "url": "/fitby/static/js/main.22217356.chunk.js"
+  },
+  {
+    "revision": "3aeea529a55e47dfeaf8",
+    "url": "/fitby/static/js/runtime-main.1b32d580.js"
+  },
+  {
+    "revision": "25a82fd4135d198a04f0ce62ed9521b4",
+    "url": "/fitby/static/media/demo.25a82fd4.png"
+  },
+  {
+    "revision": "f8c9918d06be145bc615f4114f05a3a1",
+    "url": "/fitby/static/media/logo.f8c9918d.png"
+  }
+]);
