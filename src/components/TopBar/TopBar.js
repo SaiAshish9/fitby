734c7a7190bdc0f70c3d93c10f4ee3cf0638c586
SaiAshish9 <saiashish3760@gmail.com>
2020-04-06 20:32:54 +0530

    k

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,21 +8,21 @@ const TopBar = () => {
     return (
         <div>
             
-<div  style={{height:'4vh',width:'100%',background:'#f4f4f4',display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+<div  style={{height:'4vh',width:'100%',background:'black',display:'flex',alignItems:'center',justifyContent:'space-between'}}>
 
-<h6 className="address" style={{ marginLeft:'1%',marginTop:10,color:'#F18F0E'}}> 
-tonyStark@gmail.com
+<h6 className="address" style={{ marginLeft:'1%',marginTop:10,color:'white'}}> 
+care@fitby.in
 </h6>
 
-<h6 className='phone' style={{color:'#F18F0E',textAlign:'center',margin:'auto'}}>
-    +91 123456789
+<h6 className='phone' style={{color:'white',textAlign:'center',margin:'auto'}}>
+    +91 - 7579440559
 </h6>
 
 
 <div style={{padding:'0.5rem 1.5rem',display:'flex',align:'center',justifyContent:'space-between',position:'absolute',right:10}}>
 {
     Icons.map((i,k)=>{
-      return(  <a styles={{color:'#F18F0E',margin:' 2rem'}}  key={k} href={i.link} target="_blank" rel="noopener noreferrer">
+      return(  <a styles={{color:'white',margin:' 2rem'}}  key={k} href={i.link} target="_blank" rel="noopener noreferrer">
           {i.icon}
         </a>
       )
--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -5,16 +5,16 @@ import {FaFacebookF,FaLinkedinIn,FaInstagram} from 'react-icons/fa'
 
 export default [
     {
-icon:<FaFacebookF color="#F18F0E" style={{margin:'0 0.5rem',opacity:0.7}} />,
+icon:<FaFacebookF color="white" style={{margin:'0 0.5rem',opacity:0.7}} />,
 link:"https://facebook.com/"
     },
     {
-icon:<FaInstagram color="#F18F0E" style={{margin:'0 0.5rem',opacity:0.7}}/>,
+icon:<FaInstagram color="white" style={{margin:'0 0.5rem',opacity:0.7}}/>,
 link:"https://instagram.com"
 
     },
     {
-icon:<FaLinkedinIn color="#F18F0E" style={{margin:'0 0.5rem',opacity:0.7}}/>,
+icon:<FaLinkedinIn color="white" style={{margin:'0 0.5rem',opacity:0.7}}/>,
 link:"https://linkedin.com"
     }
 ]
