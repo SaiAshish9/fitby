2a66bbc70b548e8c7c8aa227334c56481fb61de9
SaiAshish9 <saiashish3760@gmail.com>
2020-04-07 14:39:21 +0530

    added clubs

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 import Navbar from '../../components/Navbar/Navbar'
 
-import Carousel from '../../components/Carousel/Carousel'
+import Carousal from '../../components/Carousel/Carousel'
+
+import {WiDayCloudy} from 'react-icons/wi'
 
 import First from '../../components/First/First'
 
@@ -10,14 +12,49 @@ import Gallery from '../../components/Gallery/Gallery'
 
 import Blogs from '../../components/Blogs/Blogs'
 
+import Carousel from '@brainhubeu/react-carousel';
+
+import '@brainhubeu/react-carousel/lib/style.css';
+
+import {FaRegArrowAltCircleRight,FaRegArrowAltCircleLeft} from 'react-icons/fa'
+
+import C1 from '../../assets/C1.jpg'
+import C2 from '../../assets/C2.jpg'
+import C3 from '../../assets/C3.jpg'
+
 const Homepage = () => {
     return (
         <div style={{width:'100%',overflowX:'hidden'}}>
             <Navbar/>
-            <Carousel/>
+            <Carousal/>
             <First/>
 
-<div  style={{width:'100%',minHeight:'40vh',background:'#ffebd9'}}>
+<div className='container'  style={{width:'100%',minHeight:'40vh',marginTop:'10vh'}}>
+
+
+<h1 style={{textAlign:'center'}} >
+   
+<WiDayCloudy  size={100} style={{display:'bold',textAlign:'center'}} />
+<br/> 
+    Clubs
+</h1>
+
+<div style={{marginTop:'10vh'}}>
+
+<Carousel
+      arrowLeft={<FaRegArrowAltCircleLeft size={40} />}
+      arrowLeftDisabled={'.'}
+      arrowRight={ <FaRegArrowAltCircleRight size={40} />}
+      arrowRightDisabled={'.'}
+      addArrowClickHandler
+      slidesPerPage={2}
+>
+  <img src={C1} style={{width:'80%',height:'30vh',borderRadius:20}} />
+  <img src={C2} style={{width:'80%',height:'30vh',borderRadius:20}} />
+  <img src={C3} style={{width:'80%',height:'30vh',borderRadius:20}} />
+</Carousel>
+</div>
+
 
 </div>
 
