9ed9c0e1ce5a705c4fbb9d4bca180e6d7df50c80
SaiAshish9 <saiashish3760@gmail.com>
2020-04-09 00:12:15 +0530

    k

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,11 +4,19 @@ import {WiDayCloudy} from 'react-icons/wi'
 
 import C2 from '../../assets/C2.jpg'
 
+import C1 from '../../assets/C1.jpg'
+
+
+
+import Blog from '../../assets/blog.png'
+
 import './Blog.css'
 
 import Carousel from '@brainhubeu/react-carousel';
 
 import '@brainhubeu/react-carousel/lib/style.css';
+
+import {IoIosArrowForward,IoIosArrowBack} from 'react-icons/io'
 
 
 const Blogs = () => {
@@ -18,15 +26,18 @@ const data=[
 
 {
     desc:'You can always control what goes on inside',
-    date:'April 5, 2017'
+    date:'April 5, 2017',
+    image:C2
 },
 {
     desc:'Better sleep with daily yoga and meditation',
-    date:'March 30, 2017'
+    date:'March 30, 2017',
+    image:C1
 },
 {
     desc:'How to do a side crow pose in yoga',
-    date:'March 18, 2017'
+    date:'March 18, 2017',
+    image:C2
 }
 
 ]
@@ -37,27 +48,51 @@ const data=[
         <div style={{ overflowY: 'hidden',minHeight:'40vh',padding:20,marginTop:'10vh'}}>
 
 
-<h1 style={{textAlign:'center',fontFamily:"'Poppins',sans-serif"}} >
+{/* <h1 style={{textAlign:'center',fontFamily:"'Poppins',sans-serif"}} >
    
 <br/> 
     our blog
-</h1>
+</h1> */}
 
 
 {/* <div style={{display:'flex',justifyContent:'space-around',flexWrap:'wrap',alignItems:'center',marginTop:'1vh',padding:20}}> */}
 
+
+<div className='row' style={{width:'100%', minHeight:'40vh'}}>
+
+
+<div className='col-md-3' style={{textAlign:'center'}}  >
+
+<img
+
+src={Blog}
+
+style={{textAlign: 'center',width:'20rem', height:'30vh',marginTop:40}}
+/>
+
+</div>
+
+
+<div className='col-md-9'>
+
+
+
+
 <Carousel
       
-      autoPlay={2000}
+    //   autoPlay={2000}
       animationSpeed={1000}
-      infinite
-
+      arrowLeft={<IoIosArrowBack size={20} style={{color:'#00a5e3',height:'7vh',width:'3rem',boxShadow:'2px 3px 3px 2px  #00a5e3'}} />}
+      arrowLeftDisabled={'.'}
+      arrowRight={ <IoIosArrowForward size={20} style={{color:'#00a5e3',height:'7vh',width:'3rem',boxShadow:'2px 3px 3px 3px  #00a5e3'}} />  }
+      arrowRightDisabled={'.'}
+      addArrowClickHandler
       breakpoints={{
         640: {
           slidesPerPage: 1
                 },
         1200: {
-          slidesPerPage: 2
+          slidesPerPage: 1
                 },
         1800:{
             slidesPerPage: 3
@@ -73,7 +108,7 @@ const data=[
 
 
 
-<div  style={{minWidth:'45vh',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',textAlign:'center',height:'45vh',backgroundImage:`url(${C2})`,backgroundSize:'contain',backgroundPosition:'center',backgroundRepeat:'no-repeat',cursor:'pointer'}}> 
+<div  style={{minWidth:'45vh',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',textAlign:'center',height:'45vh',backgroundImage:`url(${data.image})`,backgroundSize:'contain',backgroundPosition:'center',backgroundRepeat:'no-repeat',cursor:'pointer'}}> 
 
 <a href="https://google.com/" target="_blank" rel="noopener referrer" style={{textDecoration:'none'}} >
 
@@ -93,11 +128,11 @@ Yoga Practice
 </h5>
 
 
-<p style={{marginTop:10,color:'white',marginLeft:-30,fontFamily:"'Poppins',sans-serif"}} >
+{/* <p style={{marginTop:10,color:'white',marginLeft:-30,fontFamily:"'Poppins',sans-serif"}} >
 TRX_admin  __ <span style={{marginLeft:20,fontFamily:"'Poppins',sans-serif"}}>
 {data.date}
     </span>
-</p>
+</p> */}
 </div>
 
 
@@ -112,6 +147,13 @@ TRX_admin  __ <span style={{marginLeft:20,fontFamily:"'Poppins',sans-serif"}}>
 }
 
 </Carousel>
+
+
+
+</div>
+
+</div>
+
 
 {/* </div> */}
 
